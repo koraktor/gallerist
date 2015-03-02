05f41e339bf502ae683061efcc5e40acad391c9a
Sebastian Staudt <koraktor@gmail.com>
2015-03-02 10:55:44 +0100

    Add a start script
    This allows to easily run Gallerist with a default configuration and
    using Unicorn as the server.

---
+++ lib/rack/handler/unicorn.rb
@@ -0,0 +1,34 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2015, Sebastian Staudt
+
+require 'unicorn'
+
+module Rack::Handler::Unicorn
+
+    class << self
+
+      def run(app, options = {})
+        environment = ENV['RACK_ENV'] || 'development'
+        default_host = (environment == 'development') ? 'localhost' : '0.0.0.0'
+        options[:Host] ||= default_host
+        options[:Port] ||= 8080
+
+        unicorn_options = {
+          listeners: [ '%s:%d' % [ options[:Host], options[:Port] ] ]
+        }
+
+        ::Unicorn::Launcher.daemonize!(unicorn_options) if options[:daemonize]
+        ::Unicorn::HttpServer.new(app, unicorn_options).start.join
+      end
+
+      def environment
+        ENV['RACK_ENV']
+      end
+
+    end
+
+end
+
+Rack::Handler.register :unicorn, Rack::Handler::Unicorn
