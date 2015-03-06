13a923e946306d145b39157e03088f71526b7da9
Sebastian Staudt <koraktor@gmail.com>
2015-03-06 16:30:55 +0100

    Further improve thumbnails
    Thumbnails use partials now and also display the tags of the photo, if
    any. Additionally, the `link_to` helper was improved to detect the
    current URL so tags can behave differently if a tag is currently
    displayed.

--- lib/gallerist/helpers.rb
+++ lib/gallerist/helpers.rb
@@ -10,28 +10,45 @@ module Gallerist::Helpers
   end
 
   def link_to(obj)
+    url = url_for obj
+    current = (url == request.path)
+
     classes = []
 
     case obj
     when Gallerist::Album
       link = obj.name
-      url = '/albums/%s' % [ obj.id ]
     when Gallerist::Photo
       classes << 'thumbnail'
       link = '<img src="/thumbs/%s" title="%s">' % [ obj.id, obj.tags.join(', ') ]
-      url = '/photos/%s' % [ obj.id ]
     when Gallerist::Tag
+      classes << 'label' << 'tag'
+      classes << (current ? 'label-info' : 'label-primary')
       link = obj.name
-      url = '/tags/%s' % [ URI.encode(obj.simple_name) ]
     end
 
     classes = classes.empty? ? '' : ' class="%s"' % [ classes.join(' ') ]
 
-    '<a href="%s"%s>%s</a>' % [ url, classes, link ]
+    if current
+      '<span%s>%s</span>' % [ classes, link ]
+    else
+      '<a href="%s"%s>%s</a>' % [ url_for(obj), classes, link ]
+    end
   end
 
   def title
     '%s – Gallerist' % [ @title ]
+  end
+
+  def url_for(obj)
+    case obj
+    when Gallerist::Album
+      '/albums/%s' % [ obj.id ]
+    when Gallerist::Photo
+      '/photos/%s' % [ obj.id ]
+    when Gallerist::Tag
+      '/tags/%s' % [ URI.encode(obj.simple_name) ]
+    end
   end
 
 end
