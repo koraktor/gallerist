bfb7706954585f6a540804093298323621e2d5e5
Sebastian Staudt <koraktor@gmail.com>
2015-03-16 07:35:27 +0100

    Improve sorting for persons in Photos libraries

--- lib/gallerist/models/person.rb
+++ lib/gallerist/models/person.rb
@@ -18,9 +18,13 @@ class Gallerist::Person < Gallerist::PersonModel
 
     has_many :person_photos, primary_key: 'modelId', foreign_key: 'personId'
 
+    alias_attribute :person_type, :personType
     alias_attribute :simple_name, :searchName
 
-    default_scope { select(:modelId, :name, :representativeFaceId).order(:manual_order) }
+    default_scope do
+      select(:modelId, :name, :representativeFaceId).
+      order(person_type: :desc, manual_order: :asc)
+    end
   end
 
   alias_attribute :manual_order, :manualOrder
