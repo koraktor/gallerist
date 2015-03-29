koraktor/gallerist

commit 929814c57080a4a8b6c8ec4adedaf4338e7bde19

Further improve exception handling during warmup

lib/gallerist/app.rb
@@ -21,12 +21,14 @@ class Gallerist::App < Sinatra::Base
 
   configure do
     enable :logging
+    enable :show_exceptions
 
     set :root, File.join(root, '..', '..')
 
+    set :copy_dbs, !ENV['GALLERIST_NOCOPY']
+    set :dump_errors, Proc.new { development? }
     set :library, nil
     set :library_path, ENV['GALLERIST_LIBRARY']
-    set :copy_dbs, !ENV['GALLERIST_NOCOPY']
     set :views, File.join(root, 'views')
 
     set :sprockets, Sprockets::Environment.new(root)

lib/gallerist/app/base_extensions.rb
@@ -7,7 +7,7 @@ module Gallerist::App::BaseExtensions
 
   def setup_default_middleware(builder)
     builder.use Sinatra::ExtendedRack
-    builder.use Sinatra::ShowExceptions if show_exceptions?
+    builder.use Sinatra::ShowExceptions if development?
     builder.use Gallerist::RaiseWarmupExceptions
 
     setup_logging builder

lib/gallerist/middleware/raise_warmup_exceptions.rb
@@ -18,6 +18,7 @@ class Gallerist::RaiseWarmupExceptions
       end
 
       env['rack.warmup.error'] << error
+      return 500, {}, ''
     end
 
     raise $!

commit 8a2d3cd98a5f002d6cdc3c6efb802a48fea33467

Only enable show_exceptions during warmup
This partly reverts 929814c57080a4a8b6c8ec4adedaf4338e7bde19 which
broke the error pages in production mode.

config.ru
@@ -10,10 +10,12 @@ end
 
 warmup do |app|
   error = []
+  Gallerist::App.enable :show_exceptions
   Rack::MockRequest.new(app).get '/',
     'rack.errors' => $stderr,
     'rack.warmup' => true,
     'rack.warmup.error' => error
+  Gallerist::App.disable :show_exceptions
   raise error.first unless error.empty?
 end
 

lib/gallerist/app.rb
@@ -21,7 +21,6 @@ class Gallerist::App < Sinatra::Base
 
   configure do
     enable :logging
-    enable :show_exceptions
 
     set :root, File.join(root, '..', '..')
 