b590c82557b4c4940cd5432303d5993ca01982e2
Sebastian Staudt <koraktor@gmail.com>
2015-10-24 22:46:22 +0200

    Update to Layzr.js 1.4.3
    Note that the version inside the original JS file is wrong.

--- assets/javascript/layzr.js
+++ assets/javascript/layzr.js
@@ -1,156 +1,175 @@
+/*!
+ * Layzr.js 1.4.2 - A small, fast, modern, and dependency-free library for lazy loading.
+ * Copyright (c) 2015 Michael Cavalea - http://callmecavs.github.io/layzr.js/
+ * License: MIT
+ */
+
 (function(root, factory) {
-  if(typeof define === 'function' && define.amd) {
+  if (typeof define === 'function' && define.amd) {
     define([], factory);
-  } else if(typeof exports === 'object') {
+  } else if (typeof exports === 'object') {
     module.exports = factory();
   } else {
     root.Layzr = factory();
   }
 }(this, function() {
-  'use strict';
+'use strict';
 
-  // CONSTRUCTOR
+// CONSTRUCTOR
 
-  function Layzr(options) {
-    // debounce
-    this._lastScroll = 0;
-    this._ticking = false;
+function Layzr(options) {
+  // debounce
+  this._lastScroll = 0;
+  this._ticking    = false;
 
-    // options
-    this._optionsSelector = options.selector || '[data-layzr]';
-    this._optionsAttr = options.attr || 'data-layzr';
-    this._optionsAttrRetina = options.retinaAttr || 'data-layzr-retina';
-    this._optionsAttrBg = options.bgAttr || 'data-layzr-bg';
-    this._optionsThreshold = options.threshold || 0;
-    this._optionsCallback = options.callback || null;
+  // options
+  options = options || {};
 
-    // properties
-    this._retina = window.devicePixelRatio > 1;
-    this._srcAttr = this._retina ? this._optionsAttrRetina : this._optionsAttr;
+  this._optionsContainer  = document.querySelector(options.container) || window;
+  this._optionsSelector   = options.selector || '[data-layzr]';
+  this._optionsAttr       = options.attr || 'data-layzr';
+  this._optionsAttrRetina = options.retinaAttr || 'data-layzr-retina';
+  this._optionsAttrBg     = options.bgAttr || 'data-layzr-bg';
+  this._optionsAttrHidden = options.hiddenAttr || 'data-layzr-hidden';
+  this._optionsThreshold  = options.threshold || 0;
+  this._optionsCallback   = options.callback || null;
 
-    // nodelist
-    this._nodes = document.querySelectorAll(this._optionsSelector);
+  // properties
+  this._retina  = window.devicePixelRatio > 1;
+  this._srcAttr = this._retina ? this._optionsAttrRetina : this._optionsAttr;
 
-    // call to create
-    this._create();
+  // nodelist
+  this._nodes = document.querySelectorAll(this._optionsSelector);
+
+  // scroll and resize handler
+  this._handlerBind = this._requestScroll.bind(this);
+
+  // call to create
+  this._create();
+}
+
+// DEBOUNCE HELPERS
+// adapted from: http://www.html5rocks.com/en/tutorials/speed/animations/
+
+Layzr.prototype._requestScroll = function() {
+  if(this._optionsContainer === window) {
+    this._lastScroll = window.pageYOffset;
+  }
+  else {
+    this._lastScroll = this._optionsContainer.scrollTop + this._getOffset(this._optionsContainer);
   }
 
-  // DEBOUNCE HELPERS
-  // adapted from: http://www.html5rocks.com/en/tutorials/speed/animations/
+  this._requestTick();
+};
 
-  Layzr.prototype._requestScroll = function() {
-    this._lastScroll = window.scrollY || window.pageYOffset;
-    this._requestTick();
-  };
+Layzr.prototype._requestTick = function() {
+  if(!this._ticking) {
+    requestAnimationFrame(this.update.bind(this));
+    this._ticking = true;
+  }
+};
 
-  Layzr.prototype._requestTick = function() {
-    if(!this._ticking) {
-      requestAnimationFrame(this.update.bind(this));
-      this._ticking = true;
-    }
-  };
+// OFFSET HELPER
+// remember, getBoundingClientRect is relative to the viewport
 
-  // OFFSET HELPER
-  // borrowed from: http://stackoverflow.com/questions/5598743/finding-elements-position-relative-to-the-document
+Layzr.prototype._getOffset = function(node) {
+  return node.getBoundingClientRect().top + window.pageYOffset;
+};
 
-  Layzr.prototype._getOffset = function(element) {
-    var offsetTop  = 0;
+// HEIGHT HELPER
 
-    do {
-      if(!isNaN(element.offsetTop)) {
-        offsetTop  += element.offsetTop;
-      }
-    } while (element = element.offsetParent);
+Layzr.prototype._getContainerHeight = function() {
+  return this._optionsContainer.innerHeight
+      || this._optionsContainer.offsetHeight;
+}
 
-    return offsetTop;
-  };
+// LAYZR METHODS
 
-  // LAYZR METHODS
+Layzr.prototype._create = function() {
+  // fire scroll event once
+  this._handlerBind();
 
-  Layzr.prototype._create = function() {
-    // fire scroll event once
-    this._requestScroll();
+  // bind scroll and resize event
+  this._optionsContainer.addEventListener('scroll', this._handlerBind, false);
+  this._optionsContainer.addEventListener('resize', this._handlerBind, false);
+};
 
-    // bind scroll and resize event
-    window.addEventListener('scroll', this._requestScroll.bind(this), false);
-    window.addEventListener('resize', this._requestScroll.bind(this), false);
-  };
+Layzr.prototype._destroy = function() {
+  // unbind scroll and resize event
+  this._optionsContainer.removeEventListener('scroll', this._handlerBind, false);
+  this._optionsContainer.removeEventListener('resize', this._handlerBind, false);
+};
 
-  Layzr.prototype._destroy = function() {
-    // possibly remove attributes, and set all sources?
+Layzr.prototype._inViewport = function(node) {
+  // get viewport top and bottom offset
+  var viewportTop = this._lastScroll;
+  var viewportBottom = viewportTop + this._getContainerHeight();
 
-    // unbind scroll and resize event
-    window.removeEventListener('scroll', this._requestScroll.bind(this), false);
-    window.removeEventListener('resize', this._requestScroll.bind(this), false);
-  };
+  // get node top and bottom offset
+  var nodeTop = this._getOffset(node);
+  var nodeBottom = nodeTop + this._getContainerHeight();
 
-  Layzr.prototype._inViewport = function(node) {
-    // get viewport top and bottom offset
-    var viewportTop = this._lastScroll;
-    var viewportBottom = viewportTop + window.innerHeight;
+  // calculate threshold, convert percentage to pixel value
+  var threshold = (this._optionsThreshold / 100) * window.innerHeight;
 
-    // get node top and bottom offset
-    var elementTop = this._getOffset(node);
-    var elementBottom = elementTop + node.offsetHeight;
+  // return if node in viewport
+  return nodeBottom >= viewportTop - threshold
+      && nodeTop <= viewportBottom + threshold
+      && !node.hasAttribute(this._optionsAttrHidden);
+};
 
-    // calculate threshold, convert percentage to pixel value
-    var threshold = (this._optionsThreshold / 100) * window.innerHeight;
+Layzr.prototype._reveal = function(node) {
+  // get node source
+  var source = node.getAttribute(this._srcAttr) || node.getAttribute(this._optionsAttr);
 
-    // return if element in viewport
-    return elementBottom >= viewportTop - threshold && elementBottom <= viewportBottom + threshold;
-  };
+  // set node src or bg image
+  if(node.hasAttribute(this._optionsAttrBg)) {
+    node.style.backgroundImage = 'url(' + source + ')';
+  }
+  else {
+    node.setAttribute('src', source);
+  }
 
-  Layzr.prototype._reveal = function(node) {
-    // get node source
-    var source = node.getAttribute(this._srcAttr) || node.getAttribute(this._optionsAttr);
+  // call the callback
+  if(typeof this._optionsCallback === 'function') {
+    // "this" will be the node in the callback
+    this._optionsCallback.call(node);
+  }
 
-    // set node src or bg image
-    if(node.hasAttribute(this._optionsAttrBg)) {
-      node.style.backgroundImage = 'url(' + source + ')';
-    }
-    else {
-      node.setAttribute('src', source);
-    }
+  // remove node data attributes
+  node.removeAttribute(this._optionsAttr);
+  node.removeAttribute(this._optionsAttrRetina);
+  node.removeAttribute(this._optionsAttrBg);
+  node.removeAttribute(this._optionsAttrHidden);
+};
 
-    // call the callback
-    if(typeof this._optionsCallback === 'function') {
-      // "this" will be the node in the callback
-      this._optionsCallback.call(node);
-    }
+Layzr.prototype.updateSelector = function() {
+  // update cached list of nodes matching selector
+  this._nodes = document.querySelectorAll(this._optionsSelector);
+};
 
-    // remove node data attributes
-    node.removeAttribute(this._optionsAttr);
-    node.removeAttribute(this._optionsAttrRetina);
-    node.removeAttribute(this._optionsAttrBg);
-  };
+Layzr.prototype.update = function() {
+  // cache nodelist length
+  var nodesLength = this._nodes.length;
 
-  Layzr.prototype.updateSelector = function() {
-    // update cached list of elements matching selector
-    this._nodes = document.querySelectorAll(this._optionsSelector);
-  };
+  // loop through nodes
+  for(var i = 0; i < nodesLength; i++) {
+    // cache node
+    var node = this._nodes[i];
 
-  Layzr.prototype.update = function() {
-    // cache nodelist length
-    var nodesLength = this._nodes.length;
-
-    // loop through nodes
-    for(var i = 0; i < nodesLength; i++) {
-      // cache node
-      var node = this._nodes[i];
-
-      // check if node has mandatory attribute
-      if(node.hasAttribute(this._optionsAttr)) {
-        // check if node in viewport
-        if(this._inViewport(node)) {
-          // reveal node
-          this._reveal(node);
-        }
+    // check if node has mandatory attribute
+    if(node.hasAttribute(this._optionsAttr)) {
+      // check if node in viewport
+      if(this._inViewport(node)) {
+        // reveal node
+        this._reveal(node);
       }
     }
+  }
 
-    // allow for more animation frames
-    this._ticking = false;
-  };
+  // allow for more animation frames
+  this._ticking = false;
+};
 
-  return Layzr;
+return Layzr;
 }));
